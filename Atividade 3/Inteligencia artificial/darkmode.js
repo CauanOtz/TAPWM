5948d4c0f381de6f867a347e1331a1bb599f5562
Merge <101842674+MergeTx@users.noreply.github.com>
2024-02-28 21:35:28 -0300

    Delete Atividade 3/Inteligencia artificial directory

--- Atividade 3/Inteligencia artificial/darkmode.js
+++
@@ -1,9 +0,0 @@
-const darkModeToggle = document.querySelector('.bxs-moon');
-const body = document.querySelector('body');
-
-
-darkModeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    darkModeToggle.classList.toggle('activeDark');
-    
-});
