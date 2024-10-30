e7d5836e0ac1dd26d8fbd40dae7322c66a5e549d
CAUAN ORTIZ <0031432412004@lti.fatecso.edu.br>
2024-10-30 18:05:47 -0300

    Atividade 25

--- Atividade_25/PwebNode05/Exercicios/app/routes/adicionar_professor.js
+++ Atividade_25/PwebNode05/Exercicios/app/routes/adicionar_professor.js
@@ -2,33 +2,29 @@ module.exports = function(application){
     application.get('/admin/adicionar_professor', function(req,res){
         res.render('admin/adicionar_professor');
      });
+   
      application.post('/professor/salvar', function(req,res){
    
          async function getAdcProfessor(){
-             try {
+            try {
                 var professor = req.body;
-                
+                  
                 var connection = application.config.dbConnection;
                 const pool = await connection();
    
-                var professoresModel = application.models.professorModel;
+                var professoresModel = application.models.professormodel;
    
-                //usando uma funcao de callback e informar quem devemos salvar, no caso professor
-   
-                 professoresModel.salvarProfessor(professor,pool, (error, results)=>{
-                     // após inserir redireciona o navegador para outra página
-                     // se der erro na inclusao criar um erro 500 --> nao sabe o que significa
-                    
+                professoresModel.salvarProfessor(professor,pool, (error, results)=>{
                      if(error){
                      console.log('Erro ao inserir no banco:' + error);
-                         res.status(500).send(error);
+                        res.status(500).send(error);
                      } else {
                          console.log('professor criado!!!');
-                        res.redirect('/informacao/professores');
+                         res.redirect('/admin/crud_professores');
                      }
-                 });
-             } catch (error) {
-                 console.log(error);
+                });
+            } catch (error) {
+                console.log(error);
              }
          }
          getAdcProfessor();
